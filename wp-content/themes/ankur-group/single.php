6d4b2935bbf530aafd50802a815fb5fbed0f4b9a
svirom <svirom@yahoo.com>
2017-05-25 17:03:00 +0300

    single page edits

--- wp-content/themes/ankur-group/single.php
+++ wp-content/themes/ankur-group/single.php
@@ -5,6 +5,9 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<h2><?php the_title(); ?></h2>
+			<?php if ( is_singular('post') ) { ?>
+				<p class="post_date"><?php echo __('Published:'); ?><span><?php echo get_the_date('d.m.Y'); ?></span></p>
+			<?php } ?>
 			<div><?php the_post_thumbnail('full'); ?></div>
 			<?php the_content(); ?>
 		</article>
